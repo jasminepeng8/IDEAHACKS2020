9499be77e60acac5d16eade82f9bc96a22ffe523
Isabel Dominik <icd107@live.com>
2020-01-11 15:35:15 -0800

    Beginning implementation of studyHelper (not correct)

--- studyHelper/studyHelper.ino
+++ studyHelper/studyHelper.ino
@@ -1,13 +1,29 @@
 #include "motionDetect.h"
 #include "speaker.h"
 #include "timer.h"
+#include "Distance.h"
+#include "oledDisplay.h"
 
 MotionDetect md;
+Speaker s;
+Timer t;
+Distance d;
+oledDisplay oled;
 
 void setup() {
   // put your setup code here, to run once:
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(md.PIRSensor())
+  {
+    s.on();
+    int distance = d.measureDistance();
+  }
+  else
+  {
+    s.off();
+  }
 }
