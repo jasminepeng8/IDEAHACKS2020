361ee42627becc87d5e9c5e68dc5363a48de23e5
Jasmine Peng <jaasminepeng@gmail.com>
2020-01-11 10:19:28 -0800

    edited motion detect

--- studyHelper/motionDetect.h
+++ studyHelper/motionDetect.h
@@ -1,12 +1,32 @@
-#ifndef MOTION_DETECT_H
-#define MOTION_DETECT_H
-
-#include <Arduino.h>
-
-class MotionDetect
-{
-  public:
+#ifndef MOTION_DETECT_H 
+#define MOTION_DETECT_H 
+#define Sensor 7 
+#include <Arduino.h> 
+ 
+class MotionDetect 
+{ 
+  public: 
+  
   private:
-};
+  int PIRValue = 0; 
 
-#endif
+//PIR: Passive Infrared 
+  static int PIRSensor()
+  {
+    if(digitalRead(Sensor) == HIGH) 
+    {
+        PIRValue = 1;  
+        Serial.println("Motion detected.");
+    }
+    if(digitalRead(Sensor) == LOW) 
+    {
+        PIRValue = 0; 
+        Serial.println("Motion ended."); 
+    }
+    return PIRValue; 
+  }
+  
+}; 
+ 
+#endif 
+
